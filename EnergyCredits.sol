3a00ac46dd996b376a445100129818a0bfe9d2f1
matteovaccargiu <124043561+matteovaccargiu@users.noreply.github.com>
2024-09-22 13:27:24 +0200

    Update EnergyCredits.sol

--- EnergyCredits.sol
+++ EnergyCredits.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "/Users/matteovaccargiu/Desktop/HTLC_EnergyTrading_SC/node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "/Users/matteovaccargiu/Desktop/HTLC_EnergyTrading_SC/node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract EnergyCredits is ERC20 {
     address private _owner;
