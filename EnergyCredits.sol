7e34e22a9727a24fa813e72c23e5eddb2446f732
matteovaccargiu <124043561+matteovaccargiu@users.noreply.github.com>
2024-11-04 16:30:09 +0100

    Update EnergyCredits.sol

--- EnergyCredits.sol
+++ EnergyCredits.sol
@@ -2,44 +2,34 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract EnergyCredits is ERC20 {
-    address private _owner;
-    
+contract EnergyCredits is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     constructor() ERC20("EnergyCredits", "ECR") {
-        _owner = msg.sender;
+        // Assign the administrator role to the account distributing the contract
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+
+        // Assign the role of minter to the account distributing the contract
+        _grantRole(MINTER_ROLE, msg.sender);
+
         emit OwnershipTransferred(address(0), msg.sender);
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == _owner, "Caller is not the owner");
-        _;
-    }
-
-    function owner() public view returns (address) {
-        return _owner;
-    }
-
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0), "New owner cannot be the zero address");
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
-    }
-
-    function issueCredits(address user, uint256 credits) public onlyOwner {
+    function issueCredits(address user, uint256 credits) public onlyRole(MINTER_ROLE) {
         _mint(user, credits);
     }
 
-    function transfer(address to, uint256 amount) public override returns (bool) {
-        require(to != address(0), "Invalid to address");
-        return super.transfer(to, amount);
+    // Function for assigning the minter role to an account
+    function grantMinterRole(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        grantRole(MINTER_ROLE, account);
     }
 
-    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
-        require(to != address(0), "Invalid to address");
-        return super.transferFrom(from, to, amount);
+    // Funzione per revocare il ruolo di minter da un account
+    function revokeMinterRole(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        revokeRole(MINTER_ROLE, account);
     }
-
 }
